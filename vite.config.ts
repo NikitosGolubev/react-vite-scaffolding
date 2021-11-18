ce5b1cf64f6f2a5ca7faf2a87a9fb0a505442c03
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2021-11-18 12:28:11 +0300

    Patched formatting

--- vite.config.ts
+++ vite.config.ts
@@ -6,39 +6,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
-	plugins: [
-		tsconfigPaths(),
-		react(),
-		eslintPlugin(),
-		VitePWA({
-			registerType: 'autoUpdate',
-			includeAssets: [
-				'favicon.png',
-				'robots.txt',
-				'apple-touch-icon.png',
-				'icons/*.svg',
-				'fonts/*.woff2'
-			],
-			manifest: {
-				theme_color: '#BD34FE',
-				icons: [
-					{
-						src: '/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-						purpose: 'any maskable'
-					},
-					{
-						src: '/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			}
-		}),
-		mode === 'test' &&
-			istanbul({
-				include: ['src/**/*.tsx']
-			})
-	]
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    eslintPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.png',
+        'robots.txt',
+        'apple-touch-icon.png',
+        'icons/*.svg',
+        'fonts/*.woff2'
+      ],
+      manifest: {
+        theme_color: '#BD34FE',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+    mode === 'test' &&
+      istanbul({
+        include: ['src/**/*.tsx']
+      })
+  ]
 }))
